ea0cc0a4654e74a3c6fd19643ac8f4b168b789bc
hima-del <himajaharidas77@gmail.com>
2020-07-02 22:27:37 +0530

    completed

--- task29.1_yelp_javascript/app.js
+++ task29.1_yelp_javascript/app.js
@@ -1,4 +1,3 @@
-document.querySelector('.e').addevenetlistener('DOMContentLoaded', loadData);
 //load customer
 function loadData(e) {
     const xhr = new XMLHttpRequest();
@@ -7,36 +6,58 @@ function loadData(e) {
         if (this.status === 200) {
             //console.log(this.responseText);
             const data = JSON.parse(this.responseText);
-            const output = `
-            ${data.review} reviews
-            `;
-            document.querySelector('.e').innerHTML = output;
+            // console.log(data[0].review);
+
+            data.forEach((item) => {
+                let spanId = `review${item.id}`
+                document.getElementById(spanId).textContent = `${item.review} reviews`;
+
+
+            });
+            for (i = 0; i < data.length; i++) {
+
+                addStar(data[i].id, parseFloat(data[i].stars));
+                console.log(data[i].id, parseFloat(data[i].stars));
+            }
         }
     }
     xhr.send();
 }
-//load customers
-function loadCustomers(e) {
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', '60_customers.json', true);
-    xhr.onload = function() {
-        if (this.status === 200) {
-            // console.log(this.responseText);
-            const customers = JSON.parse(this.responseText);
-            let output = '';
-            customers.forEach(function(customer) {
-                output += `
-                <ul>
-                <li>ID: ${customer.id}</li>
-                <li>Name: ${customer.name}</li>
-                <li>Company: ${customer.company}</li>
-                <li>Phone: ${customer.phone}</li>
-                </ul>
-                `;
-            });
+loadData();
+document.getElementById('one').addEventListener('click', rotateDiv);
+document.getElementById('two').addEventListener('click', rotateDiv);
+document.getElementById('three').addEventListener('click', rotateDiv);
+let arrayOrder = [1, 2, 3];
+//first click:231
+//312
+//123
+function rotateDiv() {
+    //console.log('1233');
+    let num = arrayOrder.shift();
+    console.log(num);
+    arrayOrder.push(num);
+    console.log(arrayOrder);
+    document.getElementById('one').style.order = arrayOrder[0];
+    document.getElementById('two').style.order = arrayOrder[1];
+    document.getElementById('three').style.order = arrayOrder[2];
 
-            document.getElementById('customer').innerHTML = output;
+}
+
+function addStar(id, star) {
+    const starBox = document.getElementById(id).children;
+    console.log(starBox);
+    if (Number.isInteger(star) === true) {
+        for (let i = 0; i < star; i++) {
+            console.log(starBox[i]);
+            starBox[i].classList.remove('unchecked');
+            starBox[i].classList.add('checked');
         }
+    } else {
+        for (j = 0; j < star; j++) {
+            starBox[j].classList.remove('unchecked');
+            starBox[j].classList.add('checked');
+        }
+        starBox[Math.floor(star)].classList.remove('unchecked');
+        starBox[Math.floor(star)].classList.add('fa-star-half-o');
     }
-    xhr.send();
 }
