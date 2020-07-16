7eaee5b175b818711d5ac7547d0ba3d9e82314e0
hima-del <himajaharidas77@gmail.com>
2020-07-17 00:02:41 +0530

    css of rotating keyboard

---
+++ rotating_keyboard/app.js
@@ -0,0 +1,8 @@
+const rows = document.querySelectorAll('.row');
+const letters = document.querySelectorAll('.letter');
+//console.log(row);
+//console.log(letters);
+// rows.forEach(item => {
+//     item.classList.add('horizontalselection');
+
+// });
