6f9f90b374b743fa564783daf014755672dbaa23
hima-del <himajaharidas77@gmail.com>
2020-07-17 12:08:32 +0530

    js

--- rotating_keyboard/app.js
+++ rotating_keyboard/app.js
@@ -1,8 +1,27 @@
 const rows = document.querySelectorAll('.row');
 const letters = document.querySelectorAll('.letter');
-//console.log(row);
-//console.log(letters);
-// rows.forEach(item => {
-//     item.classList.add('horizontalselection');
+const tap = document.querySelector('.tap');
+const container = document.querySelector('.container');
 
-// });
+tap.addEventListener('click', rowRotation);
+let data = [];
+data.push(rows);
+
+function rowRotation() {
+
+    addClass();
+}
+
+function addClass() {
+    for (let i = 0; i < rows.length; i++) {
+        data[0][i].classList.add('horizontalselection');
+        setTimeout(removeClass, 2000);
+    }
+
+}
+
+function removeClass() {
+    for (let i = 0; i < rows.length; i++) {
+        data[0][i].classList.remove('horizontalselection');
+    }
+}
