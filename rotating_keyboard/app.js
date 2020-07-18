45388c220b7adcb7b005093b2a0d28e93fc544b4
hima-del <himajaharidas77@gmail.com>
2020-07-19 03:07:38 +0530

    rotating keyboard

--- rotating_keyboard/app.js
+++ rotating_keyboard/app.js
@@ -8,20 +8,25 @@ let data = [];
 data.push(rows);
 
 function rowRotation() {
-
     addClass();
 }
 
 function addClass() {
+    let interval = 1;
     for (let i = 0; i < rows.length; i++) {
-        data[0][i].classList.add('horizontalselection');
-        setTimeout(removeClass, 2000);
+        //console.log(data[0][i]);
+        setTimeout(() => {
+            data[0][i].classList.add('horizontalselection');
+        }, 2000 * interval);
+        setTimeout(removeClass, 2000 * (interval + 1));
+        interval++;
     }
-
 }
 
 function removeClass() {
+    console.log('hello');
     for (let i = 0; i < rows.length; i++) {
         data[0][i].classList.remove('horizontalselection');
+
     }
 }
