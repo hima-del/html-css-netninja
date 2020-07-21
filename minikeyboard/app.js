a8ae6c9bc29ab2846b67b3d671a243b868dd2ed7
hima-del <himajaharidas77@gmail.com>
2020-07-21 22:30:47 +0530

    mini keyboard

---
+++ minikeyboard/app.js
@@ -0,0 +1,7 @@
+let row1 = document.getElementById('row1');
+let row2 = document.getElementById('row2');
+let row3 = document.getElementById('row3');
+let data = [row1, row2, row3];
+let letters = document.querySelectorAll('letter');
+let display = document.querySelectorAll('screen');
+const tap = document.querySelector('tap');
